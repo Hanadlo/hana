85f77ea3c7b3e5e9bfd830f842e38e354e3f7e0a
Hanadlo <113247952+Hanadlo@users.noreply.github.com>
2022-09-24 14:30:22 +0100

    Update script.js
    add js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-document.getElementById('noun').addEventListener('click', (event) => {
-  let element_button = document.getElementById('button');
-  element_button.innerText = 'Happy';
-  let new_a = document.createElement('a');
-  new_a.innerText = 'coding change my life';
 
-  element_button.appendChild(new_a);
-
-});
 let element_list = document.getElementById('list');
 let new_li = document.createElement('li');
 let new_a = document.createElement('a');
